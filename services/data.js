cd21b97127a962e22c1cb95e28c12b184249bec0
Luis Recchini <lrecchini@pami.org.ar>
2024-04-18 13:26:49 -0300

    test

--- services/data.js
+++ services/data.js
@@ -2,6 +2,7 @@ const { startOfDay, endOfDay, startOfHour, addHours, subHours } = require('date-
 
 
 const getMaxBetweenHours = async(collection, startHour, endHour, platform=null) => {
+    return 0;
     const startTime = new Date();
     startTime.setHours(startHour, 0, 0, 0);
 
@@ -55,7 +56,6 @@ const getMaxBetweenHours = async(collection, startHour, endHour, platform=null) 
 }
 
 const getMaxDay = async(collection, platform=null) => {
-    return 0;
     const today = new Date();
     const dayStart = startOfDay(today);
     const dayEnd = endOfDay(today);
