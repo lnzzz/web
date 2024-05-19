d7b27992ca88448189ef1a7e49b95ac1ac082414
Luis Recchini <lrecchini@pami.org.ar>
2024-05-19 04:28:08 -0300

    newnav

--- public/main.js
+++ public/main.js
@@ -11,10 +11,28 @@ function display(which) {
     } else {
         document.querySelector(`#reports`).classList.remove('d-none');
     }
+
+    if (which === 'data' || which === 'totals' || which === 'youtube' || which === 'twitch') {
+        document.querySelector("#data-totals").classList.remove('d-none');
+    } else {
+        document.querySelector("#data-totals").classList.add('d-none');
+    }
+
+    if (which === 'totals' || which === 'youtube' || which === 'twitch') {
+        document.querySelector(`#data-btn`).classList.add("active");
+        document.querySelector("#stats-data").classList.remove('d-none');
+        document.querySelector("#sum-data").classList.remove('d-none');
+    }
+
+    if (which === 'data') {
+        document.querySelector(`#${which}-btn`).classList.add("active");
+        display('totals')
+    }
 }
 
 
 
+display('data');
 display('totals');
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
