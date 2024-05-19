bbbcb0396990a8c9a6ffa1ae37ea1196c2c103fd
Luis Recchini <lrecchini@pami.org.ar>
2024-05-19 18:53:58 -0300

    cast to string

--- services/api.js
+++ services/api.js
@@ -10,8 +10,8 @@ function formatHours(date) {
   const day = String(newDate.getDate()).padStart(2, '0');
   const month = String(newDate.getMonth() + 1).padStart(2, '0'); // January is 0!
   const year = newDate.getFullYear();
-  const hours = newDate.getHours().padStart(2, '0');
-  const minutes = newDate.getMinutes().padStart(2, '0');
+  const hours = String(newDate.getHours()).padStart(2, '0');
+  const minutes = String(newDate.getMinutes()).padStart(2, '0');
   return `${day}-${month}-${year} ${hours}:${minutes}`;
 }
 
